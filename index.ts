c41b72333a835548c4d720b86b3ca9fea086bf3a
jitendra singhal <jiten.cs2010@gmail.com>
2023-07-04 18:53:09 +0530

    initial commit

--- index.ts
+++ index.ts
@@ -2,14 +2,13 @@
 import "./style.css";
 
 import testParenthesis from "./scripts/parenthesis";
-import testAnagram, { testAnagramWithSort } from "./scripts/anagram";
+import testAnagram from "./scripts/anagram";
 import { testSortByCharacterthenByNumber } from "./scripts/general";
 import testThirdMax from "./scripts/thirdmax";
 
 testSortByCharacterthenByNumber();
 testParenthesis();
 testAnagram();
-testAnagramWithSort();
 testThirdMax();
 
 // Write TypeScript code!
---
+++ scripts/bubble_sort.ts
@@ -0,0 +1,20 @@
+function bubble_sorting(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    // console.log("last element settled")
+    let swapped = false;
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+    // console.log(arr)
+    return arr;
+  }
+}
+
+bubble_sorting([2, 3, 2, 1, 5]);

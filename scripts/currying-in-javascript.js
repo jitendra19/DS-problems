232bdf79625cd9781392b2a4262f464116cdb132
Jitendra Singhal <jiten.cs2010@gmail.com>
2024-01-13 11:59:29 +0530

    Update currying-in-javascript.js

--- scripts/currying-in-javascript.js
+++ scripts/currying-in-javascript.js
@@ -1,1 +1,23 @@
+// sum function
 
+// sum(3)()()(3) = 6
+
+// function sum(a) {
+//     return function(b) {
+//         return function(c) {
+//             return function(d) {
+//                 return a+d;
+//             }
+//         }
+//     }
+// }
+
+function sumR(a) {
+    return (b) => {
+        // a+b;
+        if(!b) return a;
+        a = a+b;
+       return sumR(a);
+    }
+}
+sum(3)(3)(3)(); // 9

45f7b284514311a1bb009d8703a92e22d917aa76
jitendra <jiten.cs2010@gmail.com>
2024-03-06 15:10:34 +0000

    new approach for twosum

--- scripts/leetcode/twosum.js
+++ scripts/leetcode/twosum.js
@@ -1,4 +1,5 @@
 var twoSum = function (nums, target) {
+  // // Brute Force
   // let sum = [];
   // for(let i =0;i<nums.length;i++) {
   //     for(let j=i+1; j<nums.length;j++) {
@@ -11,6 +12,7 @@ var twoSum = function (nums, target) {
   // }
   // return sum;
 
+  // // with Map
   // const mapping = new Map();
   // for(let i=0;i<nums.length;i++) {
   //     const c = target - nums[i];
@@ -21,14 +23,24 @@ var twoSum = function (nums, target) {
   // }
   // return [];
 
-  const mapping = {};
-  for (let i in nums) {
-    mapping[nums[i]] = i;
-  }
-  for (let i in nums) {
-    const c = target - nums[i];
-    if (mapping[c] && mapping[c] !== i) {
-      return [i, mapping[c]];
+  // // with extra object
+  // const mapping = {};
+  // for (let i in nums) {
+  //   mapping[nums[i]] = i;
+  // }
+  // for (let i in nums) {
+  //   const c = target - nums[i];
+  //   if (mapping[c] && mapping[c] !== i) {
+  //     return [i, mapping[c]];
+  //   }
+  // }
+  // return [];
+
+  // without object
+  for (let i in a) {
+    let c = t - a[i];
+    if (a.indexOf(c) > -1 && a.indexOf(c) != i) {
+      return [a[i], c];
     }
   }
   return [];
